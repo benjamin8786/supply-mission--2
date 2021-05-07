133c5c6677cee2e828db323b1a033fc4c2d3b92f
benjamin8786 <79914158+benjamin8786@users.noreply.github.com>
2021-05-07 17:39:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,78 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground,wall1,wall2,wall3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+    wall1=createSprite(400,654,200,15)
+    wall1.shapeColor=color("orange")
+
+	wall2=createSprite(300,561,15,200)
+    wall2.shapeColor=color("orange")
+
+	wall3=createSprite(500,561,15,200)
+    wall3.shapeColor=color("orange")
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2,650,width,10,{isStatic:true} );
+ 	World.add(world, ground);
+
+	wall1 = Bodies.rectangle(400,650,200,15,{isStatic:true} );
+ 	World.add(world, wall1);
+
+	wall2 = Bodies.rectangle(300,557,15,200,{isStatic:true} );
+ 	World.add(world, wall2);
+
+    wall3 = Bodies.rectangle(500,557,15,200,{isStatic:true} );
+ 	World.add(world, wall3);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+    
+  }
+}
